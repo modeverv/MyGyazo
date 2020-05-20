2d5fc9c0c8102406a7aa5b336ebba70609ca0bdc
nora inu <modeverv@gmail.com>
2020-05-21 00:47:43 +0900

    作成日時 descで並び替え

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
-$filenames = glob("data/*.png");
+$filenames = [];
+foreach(glob("data/*.png") as $entry){
+    $filenames[] = $entry;
+}
+function compare($a,$b){
+  if(filemtime($a) == filemtime($b)){
+    return 0;
+  }
+  return (filemtime($a) < filemtime($b)) ? 1 : -1;
+}
+usort($filenames,'compare');
 ?>
+<style>
+* { margin: 0;padding:0}
+li { list-style : none;}
+img { opacity: 0.8}
+img:hover { opacity: 1}
+</style>
 <?php foreach($filenames as $filename) { ?>
     <li><a target="_blank" href="<?php echo $filename ?>" ><img src="<?php echo $filename ?>" /></a>
 <?php } ?>
