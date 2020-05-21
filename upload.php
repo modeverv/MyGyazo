09d8fcafb4ed85ca4692bca1bf20c663c27d77c8
nora inu <modeverv@gmail.com>
2020-05-21 21:46:38 +0900

    ハードコーディング排除

--- upload.php
+++ upload.php
@@ -3,5 +3,4 @@ $id=$_REQUEST["id"];
 $imagedata = file_get_contents($_FILES['imagedata']['tmp_name']);
 $filename = "data/" . md5($imagedata) . ".png";
 file_put_contents ($filename, $imagedata);
-echo "https://gyazo.lovesaemi.daemon.asia/" . $filename; 
-
+echo (empty($_SERVER["HTTPS"]) ? "http://" : "https://") . $_SERVER["HTTP_HOST"] . "/"  . $filename; 
